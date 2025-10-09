0fb74bb6ce534f81a7203115a8628d2a1141e026
shastriarya <kumarbharatenduarya@gmail.com>
2025-10-09 19:39:40 +0530

    add backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const main = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB Atlas successfully...");
   } catch (error) {
     console.error("MongoDB Atlas connection failed:", error.message);
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
